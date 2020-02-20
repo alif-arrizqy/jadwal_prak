5d964ce07691bca628c8d58baf93bd28d2f8dd7e
Yoga17 <triyoga.ginanjar.p@gmail.com>
2020-02-20 23:53:03 +0700

    koneksi ubah

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$koneksi = mysqli_connect("localhost","root","","jadwal_prak");
+$koneksi = mysqli_connect("mysql.idhostinger.com","u177600864_root","jadwal_prak","u177600864_jadwal_prak");
 //tes
 if(mysqli_connect_errno()){
 	echo "koneksi gagal : ".mysqli_connect_error();
