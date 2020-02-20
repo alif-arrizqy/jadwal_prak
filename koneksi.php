296dade02f34ad849844aeff0bb215b153f6bc2e
alif-arrizqy <alif.arrizqy20@gmail.com>
2020-02-21 00:19:59 +0700

    cobain deploynya

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 $koneksi = mysqli_connect("mysql.idhostinger.com","u177600864_root","jadwal_prak","u177600864_jadwal_prak");
-//tes
+//tes coba deploy
 if(mysqli_connect_errno()){
 	echo "koneksi gagal : ".mysqli_connect_error();
 }
